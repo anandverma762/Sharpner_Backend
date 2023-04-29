075d0ff46166b9d322c7e07d93d54b733c8922ab
anandverma762@gmail.com <anandverma762@gmail.com>
2023-04-29 16:08:51 +0530

    hi

--- serverfunction.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const rt = require('./routes')
-
-// console.log(rt.key)
-console.log(rt.sometext)
-const serv=http.createServer(rt.handler)
-
-serv.listen(4000);
-console.log("Server Startet....")
